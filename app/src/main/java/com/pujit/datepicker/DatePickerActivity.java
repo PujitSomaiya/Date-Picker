3e142d5a5eb0f2253602ea995ad803239f1c5ee6
PujitSomaiya <37361593+PujitSomaiya@users.noreply.github.com>
2020-04-28 16:49:11 +0530

    -- almost completed

--- app/src/main/java/com/pujit/datepicker/DatePickerActivity.java
+++ app/src/main/java/com/pujit/datepicker/DatePickerActivity.java
@@ -1,21 +1,26 @@
 package com.pujit.datepicker;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
-import com.michaldrabik.classicmaterialtimepicker.CmtpDateDialogFragment;
+import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
+import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
 import com.michaldrabik.classicmaterialtimepicker.CmtpTimeDialogFragment;
 import com.michaldrabik.classicmaterialtimepicker.CmtpTimePickerView;
 import com.michaldrabik.classicmaterialtimepicker.OnTime12PickedListener;
 import com.michaldrabik.classicmaterialtimepicker.model.CmtpTime12;
 import com.vivekkaushik.datepicker.DatePickerTimeline;
 import com.vivekkaushik.datepicker.OnDateSelectedListener;
+
+import org.joda.time.DateTime;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -24,83 +29,58 @@ import java.util.Date;
 
 import static androidx.core.util.Preconditions.checkArgument;
 
-public class DatePickerActivity extends AppCompatActivity {
+public class DatePickerActivity extends AppCompatActivity implements View.OnClickListener, DatePickerListener {
 
-    private DatePickerTimeline dpDateLine;
-    private TextView tvDate,tvFrom,tvFromm,tvTo,tvToo;
+    private HorizontalPicker dpDateLine;
+    private TextView tvDate, tvFrom, tvFromm, tvTo, tvToo;
     private Date date;
+    private CmtpTime12 minCheck;
     private FragmentManager fragmentManager;
-    private CmtpTimeDialogFragment cmtpTimeDialogFragment=CmtpTimeDialogFragment.newInstance();
+    private CmtpTimePickerView cmtpTimePickerView;
+    private CmtpTimeDialogFragment cmtpTimeDialogFragment = CmtpTimeDialogFragment.newInstance();
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
             initControls();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+
     }
 
-    private void initControls() throws ParseException {
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void initControls() {
 
         initListeners();
         datePickerTimeline();
     }
 
 
-
-
-    private void datePickerTimeline() throws ParseException {
+    private void datePickerTimeline()  {
         date = (Calendar.getInstance().getTime());
-        dpDateLine.setInitialDate(date.getYear(), date.getMonth(), date.getDate());
+//        dpDateLine.setDate(DateTime.now());
         dpDateLine.setSelected(true);
-        dpDateLine.setOnDateSelectedListener(new OnDateSelectedListener() {
-            @Override
-            public void onDateSelected(int year, int month, int day, int dayOfWeek) {
-                Calendar calendar;
-                calendar = Calendar.getInstance();
-                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d");
-                calendar.set(year, month, day);
-                Date date=new Date(year,month,day);
-                String d = dateFormat.format(date);
-                tvDate.setText(d+getDayOfMonthSuffix(day)+",");
-            }
-
-            @Override
-            public void onDisabledDateSelected(int year, int month, int day, int dayOfWeek, boolean isDisabled) {
-
-            }
-        });
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private void initListeners() {
         dpDateLine = findViewById(R.id.dpDateLine);
+        dpDateLine.setListener(this).init();
+        dpDateLine.setDate(new DateTime());
         tvDate = findViewById(R.id.tvDate);
         tvFromm = findViewById(R.id.tvFromm);
         tvToo = findViewById(R.id.tvToo);
         tvTo = findViewById(R.id.tvTo);
         tvFrom = findViewById(R.id.tvFrom);
-        CmtpTimePickerView cmtpTimePickerView = findViewById(R.id.cmtpTimePickerView);
-        fragmentManager=this.getSupportFragmentManager();
-        cmtpTimeDialogFragment.setInitialTime12(10,10, CmtpTime12.PmAm.AM);
-        tvFrom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tvFrom();
-            }
-        });
-        tvTo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tvTo();
-            }
-        });
-
+        cmtpTimePickerView  = findViewById(R.id.cmtpTimePickerView);
+        fragmentManager = this.getSupportFragmentManager();
+        cmtpTimeDialogFragment.setInitialTime12(10, 10, CmtpTime12.PmAm.AM);
+        tvTo.setOnClickListener(this);
+        tvFrom.setOnClickListener(this);
     }
+
     @SuppressLint("RestrictedApi")
     String getDayOfMonthSuffix(final int n) {
         checkArgument(n >= 1 && n <= 31, "illegal day of month: " + n);
@@ -108,37 +88,97 @@ public class DatePickerActivity extends AppCompatActivity {
             return "th";
         }
         switch (n % 10) {
-            case 1:  return "st";
-            case 2:  return "nd";
-            case 3:  return "rd";
-            default: return "th";
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
         }
     }
 
 
-// push local git
     private void tvFrom() {
 
 
         cmtpTimeDialogFragment.setOnTime12PickedListener(new OnTime12PickedListener() {
             @Override
             public void onTimePicked(CmtpTime12 cmtpTime12) {
-                tvFromm.setText(String.valueOf(cmtpTime12)+"      to");
+                minCheck=cmtpTime12;
+                tvFromm.setText(String.valueOf(cmtpTime12) + "      to");
                 tvFrom.setText(String.valueOf(cmtpTime12));
+                if (String.valueOf(cmtpTime12.getHour()).equals("12")){
+                    tvTo.setText("1:"+ String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                    tvToo.setText("1:"+ String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                }else {
+                    tvTo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                    tvToo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                }
+                cmtpTimePickerView.setTime(cmtpTime12);
             }
         });
-        cmtpTimeDialogFragment.show( fragmentManager,"From");
+        cmtpTimeDialogFragment.show(fragmentManager, "From");
     }
 
     private void tvTo() {
-        cmtpTimeDialogFragment.setInitialTime12(10,10, CmtpTime12.PmAm.AM);
         cmtpTimeDialogFragment.setOnTime12PickedListener(new OnTime12PickedListener() {
             @Override
             public void onTimePicked(CmtpTime12 cmtpTime12) {
-                tvToo.setText(String.valueOf(cmtpTime12));
-                tvTo.setText(String.valueOf(cmtpTime12));
+                if (String.valueOf(cmtpTime12).equals(tvFrom.getText().toString())) {
+                    if (String.valueOf(cmtpTime12.getHour()).equals("12")){
+                        tvTo.setText("1:"+ String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                        tvToo.setText("1:"+ String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                    }else {
+                        if (Integer.parseInt(String.valueOf(cmtpTime12.getHour()))<(Integer.parseInt(String.valueOf(minCheck.getHour())))){
+                            tvTo.setText(String.valueOf(minCheck.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ " :") + String.valueOf(cmtpTime12.getPmAm()));
+                            tvToo.setText(String.valueOf(minCheck.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ ":") + String.valueOf(cmtpTime12.getPmAm()));
+                        }else {
+                            if (Integer.parseInt(String.valueOf(cmtpTime12.getMinute()))<(Integer.parseInt(String.valueOf(minCheck.getMinute())))){
+                                tvTo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ ":") + String.valueOf(cmtpTime12.getPmAm()));
+                                tvToo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ ":") + String.valueOf(cmtpTime12.getPmAm()));
+                            }else {
+                                tvTo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                                tvToo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                            }
+                        }
+                    }
+                }else if (String.valueOf(cmtpTime12.getHour()).equals("12")){
+                    tvTo.setText("1:"+ String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                    tvToo.setText("1:"+ String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                } else if (Integer.parseInt(String.valueOf(cmtpTime12.getHour()))<(Integer.parseInt(String.valueOf(minCheck.getHour())))){
+                        tvTo.setText(String.valueOf(minCheck.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ " :") + String.valueOf(cmtpTime12.getPmAm()));
+                        tvToo.setText(String.valueOf(minCheck.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ ":") + String.valueOf(cmtpTime12.getPmAm()));
+                    }else if (Integer.parseInt(String.valueOf(cmtpTime12.getMinute()))<(Integer.parseInt(String.valueOf(minCheck.getMinute())))&&Integer.parseInt(String.valueOf(cmtpTime12.getHour()))<(Integer.parseInt(String.valueOf(minCheck.getHour())))){
+                            tvTo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ ":") + String.valueOf(cmtpTime12.getPmAm()));
+                            tvToo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(minCheck.getMinute()+ ":") + String.valueOf(cmtpTime12.getPmAm()));
+                        }else {
+                            tvTo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm()));
+                            tvToo.setText(String.valueOf(cmtpTime12.getHour() + 1 + ":") + String.valueOf(cmtpTime12.getMinute() + ":") + String.valueOf(cmtpTime12.getPmAm())); }
+
             }
         });
-        cmtpTimeDialogFragment.show( fragmentManager,"T0");
+        cmtpTimeDialogFragment.show(fragmentManager, "To");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.tvTo) {
+            tvTo();
+        } else if (view.getId() == R.id.tvFrom) {
+            tvFrom();
+        }
+    }
+
+    @Override
+    public void onDateSelected(DateTime dateSelected) {
+        Calendar calendar;
+        calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d");
+//        calendar.set(year, month, day);
+        Date date = new Date(dateSelected.getYear(),dateSelected.getMonthOfYear()-1,dateSelected.getDayOfMonth());
+        String d = dateFormat.format(date);
+        tvDate.setText(d + getDayOfMonthSuffix(dateSelected.getDayOfMonth()) + ",");
     }
 }
